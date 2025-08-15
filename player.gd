0ccba6fe10148dacc3aae85d57f4a4b974d9ed05
Prason <prasonkc07@gmail.com>
2025-08-15 23:13:39 +0545

    Added animations for foxy

--- player.gd
+++ player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
+@onready var anim = get_node("AnimationPlayer")
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,13 +13,25 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		anim.play("Jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
+		if direction == -1:
+			get_node("AnimatedSprite2D").flip_h = true
+		elif direction == 1:
+			get_node("AnimatedSprite2D").flip_h = false
+			
 		velocity.x = direction * SPEED
+		if velocity.y ==0:
+			anim.play("Run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if velocity.y ==0:
+			anim.play("Idle")
+	if velocity.y > 0:
+		anim.play("Fall")
 
 	move_and_slide()
